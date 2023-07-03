c72afeefd172a93c88cd9e6744fdd450c370480e
fajri.illahi <fajri.illahi@ifg-life.id>
2023-07-03 17:34:57 +0700

    Dockerization using non standard quarkus dockerfile worked locally and tried ObjectMapper class

--- product-service/src/main/java/com/beyonder/ExampleResource.java
+++ product-service/src/main/java/com/beyonder/ExampleResource.java
@@ -1,16 +1,50 @@
 package com.beyonder;
 
+import com.beyonder.dto.request.AddProductReqDTO;
+import com.beyonder.model.Product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vertx.core.json.JsonObject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
+import javax.print.attribute.standard.Media;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Path("/hello")
 public class ExampleResource {
+    List<Product> productList = new ArrayList<>();
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addProduct(AddProductReqDTO addProductReqDTO) throws JsonProcessingException {
+        ObjectMapper omm = new ObjectMapper();
+        String jsn = omm.writeValueAsString(addProductReqDTO);
+        System.out.println(jsn);
+        Product product = new Product();
+        product.setProductName(addProductReqDTO.getName());
+        product.setProductCode(addProductReqDTO.getCode());
+        productList.add(product);
+
+        for(Product prod: productList){
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(prod);
+            System.out.println(json);
+        }
+        return Response.ok().entity(productList).build();
     }
 }
---
+++ product-service/src/main/java/com/beyonder/model/Product.java
@@ -0,0 +1,40 @@
+package com.beyonder.model;
+
+public class Product {
+    private String productName;
+    private String productCode;
+    private Double productPrice;
+    private Integer productStock;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Integer getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(Integer productStock) {
+        this.productStock = productStock;
+    }
+}
